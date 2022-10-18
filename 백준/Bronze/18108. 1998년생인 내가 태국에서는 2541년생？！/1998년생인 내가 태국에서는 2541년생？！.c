fb4c8dc1a60c3f4cef22a5ecab17863b2a7dfb0c
Getbawhi <95276794+getbawhi@users.noreply.github.com>
2022-10-18 11:59:45 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    
+    int num;
+    int result =0;
+    
+    scanf("%d", &num);
+    
+    result = num - 543;
+    printf("%d", result);
+    
+    
+    return 0;
+}
