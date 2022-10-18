8013371a6fc7c28e5a9f1325ebfde373d14b31c1
Getbawhi <95276794+getbawhi@users.noreply.github.com>
2022-10-18 12:06:31 +0900

    [Bronze V] Title: NFC West vs North, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10170. NFC West vs North/NFC West vs North.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(){
+    
+    printf("NFC West       W   L  T\n");
+    printf("-----------------------\n");
+    printf("Seattle        13  3  0\n");
+    printf("San Francisco  12  4  0\nArizona        10  6  0\nSt. Louis      7   9  0\n\n");
+    printf("NFC North      W   L  T\n");
+    printf("-----------------------\n");
+    printf("Green Bay      8   7  1\nChicago        8   8  0\nDetroit        7   9  0\nMinnesota      5  10  1\n");
+}
