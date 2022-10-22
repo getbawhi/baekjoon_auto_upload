59993904f613108251822f60338d0892a2691854
Getbawhi <95276794+getbawhi@users.noreply.github.com>
2022-10-22 17:55:23 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    
+    long int a,b,c;
+    long int result =0;
+    scanf("%ld %ld %ld", &a, &b, &c);
+    result = a+b+c;
+    
+    printf("%ld", result);
+    return 0;
+}
