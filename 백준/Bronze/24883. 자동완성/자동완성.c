48d0905dcc4df882c063ad791a94cbe3fde1b5c3
Getbawhi <95276794+getbawhi@users.noreply.github.com>
2022-11-04 10:01:37 +0900

    [Bronze IV] Title: 자동완성, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#include <string.h>
+
+int main() {
+
+    char input;
+
+    scanf("%c", &input);
+
+    if (input == 'n')
+    {
+        printf("Naver D2");
+
+    }
+
+    else if (input == 'N')
+    {
+        printf("Naver D2");
+    }
+
+
+    else
+    {
+        printf("Naver Whale");
+    }
+
+
+    return 0;
+}
+
